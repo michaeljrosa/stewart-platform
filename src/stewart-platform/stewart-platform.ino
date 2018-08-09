f992af2a52ce98950ce9b6b859441bf829c748d3
Unknown <michael.rosa.qwerty@gmail.com>
2018-08-09 16:22:01 -0400

    Added motor drivers
    Added motor driver functionality and removed relay functionality in pin/function definitions.
    Added more functions to set the platform orientation e.g. setPlatformLengths. 
    Added more debugging print lines.
    Adjusted delays for calibration with define statements. Also adjusted second probe speed to half speed -- might change this to quarter speed.
    Added simple loop to make platform rise and fall to 0.1 and 0.9 of max actuator lengths.

--- src/stewart-platform/stewart-platform.ino
+++ src/stewart-platform/stewart-platform.ino
@@ -4,38 +4,45 @@
  * Authors: Leroy Sibanda, Michael Rosa
  * Date: August 2018
  * 
- * Program to test and calibrate an Stewart platform based on an Arduino Micro and relays
- * Will be updated in the future to use an Arduino Mega and motor drivers, with  the
- * additional functionality that those entail (e.g. PWM)
+ * Program to test and calibrate an Stewart platform based on an Arduino Mega and motor drivers
  */
 
 // ------ constants and defines ------ //
 // graphing 
 #define IS_GRAPHING true  // graph sensor values?
-#define IS_RAW_DATA true  // graph the raw sensor data? if not, will graph filtered
-#define WHICH_ACTUATOR 1  // which actuator to graph data from, 1-6
+#define IS_RAW_DATA false  // graph the raw sensor data? if not, will graph filtered
+#define WHICH_ACTUATOR 0  // which actuator to graph data from, 0-5
 
-// control pins
-#define FORW_1  12  // 1st linear actuator forward enable
-#define BACK_1  11  // 1st linear actuator backward enable
-#define FORW_2  10  // 2nd linear actuator forward enable
-#define BACK_2   9  // 2nd linear actuator backward enable
-#define FORW_3   8  // 3rd linear actuator forward enable
-#define BACK_3   7  // 3rd linear actuator backward enable
-#define FORW_4   6  // 4th linear actuator forward enable
-#define BACK_4   5  // 4th linear actuator backward enable
-#define FORW_5   4  // 5th linear actuator forward enable
-#define BACK_5   3  // 5th linear actuator backward enable
-#define FORW_6   0  // 6th linear actuator forward enable
-#define BACK_6   1  // 6th linear actuator backward enable
+// control/feedback pins
+#define IN1_A1  32
+#define IN2_A1  33
+#define ENA_A1   8
+#define FEED_A1 A0  
 
-// feedback pins
-#define FEED_1  A0  // 1st linear actuator feedback
-#define FEED_2  A1  // 2nd linear actuator feedback
-#define FEED_3  A2  // 3rd linear actuator feedback
-#define FEED_4  A3  // 4th linear actuator feedback
-#define FEED_5  A4  // 5th linear actuator feedback
-#define FEED_6  A5  // 6th linear actuator feedback
+#define IN1_A2  30
+#define IN2_A2  31
+#define ENA_A2   9
+#define FEED_A2 A1
+
+#define IN1_B1  28
+#define IN2_B1  29
+#define ENA_B1  10
+#define FEED_B1 A2
+
+#define IN1_B2  26
+#define IN2_B2  27
+#define ENA_B2  11
+#define FEED_B2 A3
+
+#define IN1_C1  24
+#define IN2_C1  25
+#define ENA_C1  12
+#define FEED_C1 A4
+
+#define IN1_C2  22
+#define IN2_C2  23
+#define ENA_C2  13
+#define FEED_C2 A5
 
 // constants for sensor readings/movement
 #define SMOOTH      5  // smoothing factor, 5-50
@@ -44,13 +51,15 @@
 // other constants
 #define NUM_CALIB_STAGES 9
 #define NUM_ACTUATORS 6
-
+#define LENGTH 12  // inches
+#define SPEED 0.5  // inches/sec
 
 // ------ classes ------ //
 class Actuator {
   private:
-    const byte extendPin;
-    const byte retractPin;
+    const byte in1Pin;
+    const byte in2Pin;
+    const byte enaPin;
     const byte feedbackPin;
     
     int maxPosition;
@@ -65,38 +74,57 @@ class Actuator {
     int readings[SMOOTH];
     int index;
     int total;
-    
+
     void extend() {
-      digitalWrite(extendPin, LOW);
-      digitalWrite(retractPin, HIGH);
+      extend(255);
+    }
+    
+    void extend(uint8_t pwm) {
+      digitalWrite(in1Pin, HIGH);
+      digitalWrite(in2Pin, LOW);
+      analogWrite(enaPin, pwm);
     }
 
     void retract() {
-      digitalWrite(extendPin, HIGH);
-      digitalWrite(retractPin, LOW);
+      retract(255);
+    }
+    
+    void retract(uint8_t pwm) {
+      digitalWrite(in1Pin, LOW);
+      digitalWrite(in2Pin, HIGH);
+      analogWrite(enaPin, pwm);
     }
 
-    void halt() {
-      digitalWrite(extendPin, HIGH);
-      digitalWrite(retractPin, HIGH);
+    void off() { // leave motor floating
+      digitalWrite(in1Pin, HIGH);
+      digitalWrite(in2Pin, HIGH);
+      digitalWrite(enaPin, LOW);
+    }
+
+    void brake() {
+      digitalWrite(in1Pin, LOW);
+      digitalWrite(in2Pin, LOW);
+      digitalWrite(enaPin, LOW);
     }
 
     void readPosition() {
       total -= readings[index];                // subtract the last value
       rawPosition = analogRead(feedbackPin);   // read the sensor
       readings[index] = rawPosition;     // record the reading
-      total += rawPosition;              // add the value 
+      total += readings[index];              // add the value
       filtPosition = total / SMOOTH;     // average reading and update the filtered position
-      
-      if (index++ >= SMOOTH) {  // reset index if at end of array
+
+      index++;
+      if (index >= SMOOTH) {  // reset index if at end of array
         index = 0;
       }
     }
 
   public:    
-    Actuator(byte attachToExtendPin, byte attachToRetractPin, byte attachToFeedbackPin) :
-      extendPin(attachToExtendPin),
-      retractPin(attachToRetractPin),
+    Actuator(byte attachToIn1Pin, byte attachToIn2Pin, byte attachToEnaPin, byte attachToFeedbackPin) :
+      in1Pin(attachToIn1Pin),
+      in2Pin(attachToIn2Pin),
+      enaPin(attachToEnaPin),
       feedbackPin(attachToFeedbackPin) {
          
       isCalibrated = false;
@@ -113,22 +141,32 @@ class Actuator {
     }
     
     void setup() {
-      pinMode(extendPin, OUTPUT);
-      pinMode(retractPin, OUTPUT);
+      pinMode(in1Pin, OUTPUT);
+      pinMode(in2Pin, OUTPUT);
+      pinMode(enaPin, OUTPUT);
       pinMode(feedbackPin, INPUT);
-      halt();
+      off();
     }
     
     void loop() {
       readPosition();
       if (isCalibrated && !isReady) {
-        if (filtPosition <= targetPosition + TOLERANCE || filtPosition >= targetPosition - TOLERANCE) {
-          halt();
+        /*
+        Serial.print("Upper limit: "); Serial.println(targetPosition + TOLERANCE);
+        Serial.print("Lower limit: "); Serial.println(targetPosition - TOLERANCE);
+        Serial.print("Current position: "); Serial.println(filtPosition);
+        Serial.print("Raw position: "); Serial.println(rawPosition);
+        */
+        if (filtPosition <= targetPosition + TOLERANCE && filtPosition >= targetPosition - TOLERANCE) { 
+          //Serial.println("stop");
+          brake();
           isReady = true;
-        } else if (filtPosition > targetPosition + TOLERANCE) {
-          retract();
-        } else if (filtPosition < targetPosition - TOLERANCE) {
+        } else if (filtPosition > targetPosition + TOLERANCE) { 
+          //Serial.println("extend"); 
           extend();
+        } else if (filtPosition < targetPosition - TOLERANCE) { 
+          //Serial.println("retract");
+          retract();
         }
       }
     }
@@ -147,13 +185,17 @@ class Actuator {
     bool calibrate() {
       switch (calibrationStage) {
         case 0: 
-        case 3:
         case 7: extend();
                 break;
 
+        case 3: extend(127);
+                break;
+
         case 2:
-        case 5:
-        case 8: retract();
+        case 5: retract();
+                break;
+        
+        case 8: retract(127);
                 break;
                 
         case 1:
@@ -161,28 +203,29 @@ class Actuator {
         case 6:
         case 9: for(int i = 0; i < SMOOTH; i++)
                   readPosition();
-                halt();
+                brake();
                 break;
         
         default:break;
       }
 
       switch (calibrationStage) {
-        case 1: maxPosition = filtPosition;
+        case 1: minPosition = filtPosition;
                 break;
 
-        case 4: maxPosition = min(maxPosition, filtPosition);
+        case 4: minPosition = max(minPosition, filtPosition);
                 break;
 
-        case 6: minPosition = filtPosition;
+        case 6: maxPosition = filtPosition;
                 break;
 
-        case 9: minPosition = max(minPosition, filtPosition);
+        case 9: maxPosition = min(maxPosition, filtPosition);
                 break;
 
         default: break;
       }
-      if(calibrationStage++ > NUM_CALIB_STAGES) {
+      calibrationStage++;
+      if(calibrationStage > NUM_CALIB_STAGES) {
         calibrationStage = 0;
         isCalibrated = true;
         isReady = true;
@@ -191,10 +234,10 @@ class Actuator {
       return isCalibrated;
     }
 
-    void goToPosition(double relativePosition) {
-      if (isCalibrated && isReady) {
+    void setLength(double relativeLength) {
+      if (isCalibrated && isReady && relativeLength >= 0 && relativeLength <= 1) {
         isReady = false;
-        targetPosition = minPosition + (maxPosition - minPosition) * relativePosition;
+        targetPosition = minPosition + (maxPosition - minPosition) * relativeLength;
       }
     }
 
@@ -226,18 +269,20 @@ class Actuator {
 
 class StewartPlatform {
   bool isCalibrated;
+  bool isReady;
   Actuator actuators[NUM_ACTUATORS] = {
-    Actuator(FORW_1, BACK_1, FEED_1),
-    Actuator(FORW_2, BACK_2, FEED_2),
-    Actuator(FORW_3, BACK_3, FEED_3),
-    Actuator(FORW_4, BACK_4, FEED_4),
-    Actuator(FORW_5, BACK_5, FEED_5),
-    Actuator(FORW_6, BACK_6, FEED_6),
+    Actuator(IN1_A1, IN2_A1, ENA_A1, FEED_A1),
+    Actuator(IN1_A2, IN2_A2, ENA_A2, FEED_A2),
+    Actuator(IN1_B1, IN2_B1, ENA_B1, FEED_B1),
+    Actuator(IN1_B2, IN2_B2, ENA_B2, FEED_B2),
+    Actuator(IN1_C1, IN2_C1, ENA_C1, FEED_C1),
+    Actuator(IN1_C2, IN2_C2, ENA_C2, FEED_C2),
   };
   
   public:
     StewartPlatform() {
       isCalibrated = false;
+      isReady = false;
     }
 
     void setup() {
@@ -254,39 +299,45 @@ class StewartPlatform {
     }
 
     void calibrate() {
-      for (int stage = 0; stage < NUM_CALIB_STAGES; stage++) {
+      for (int stage = 0; stage <= NUM_CALIB_STAGES; stage++) {
         for (int i = 0; i < NUM_ACTUATORS; i++) {
           actuators[i].calibrate();
         }
 
         switch (stage) {
           case 0: 
-          case 5: delay(30000); // wait 30 secs for actuators to fully extend/retract fully
+          case 5: delay(LENGTH / SPEED * 1000 + 1000); // wait for actuators to fully extend/retract
                   break;
 
           case 2:
-          case 7: delay(3000); // wait 3 secs for actuators to extend/retract to prepare sanity check
+          case 7: delay(0.125 * LENGTH / SPEED * 1000); // wait 2 secs for actuators to extend/retract to prepare second check
                   break;
 
           case 3:
-          case 8: delay(5000); // wait 5 secs for actuators to extend/retract to make sanity check
-                  
-          case 1: 
-          case 4:
-          case 6:
-          case 9: delay(500); // changeover pause 
+          case 8: delay(0.125 * LENGTH / (SPEED * 0.5) * 1000 + 1000); // wait 5 secs for actuators to extend/retract to make second check
                   break;
+                  
+          default: break;
         }
       }
       isCalibrated = true;
-       for (int i = 0; i < NUM_ACTUATORS; i++) {
-          actuators[i].goToPosition(0.5);
-       }
+      isReady = true;
     }
 
+    bool setPlatformLengths(double (&lengths)[6]) {
+      if (getPlatformReady()) {
+        for (int i = 0; i < NUM_ACTUATORS; i++) {
+          actuators[i].setLength(lengths[i]);
+        }
+        return true;
+      } else {
+        return false;
+      }
+    }
+    
     int getActuatorRawPosition(int actuator) {
       if (actuatorIsValid(actuator)) {
-        return actuators[actuator - 1].getRawPosition();
+        return actuators[actuator].getRawPosition();
       } else {
         return 0;
       }
@@ -294,7 +345,15 @@ class StewartPlatform {
 
     int getActuatorPosition(int actuator) {
       if (actuatorIsValid(actuator)) {
-        return actuators[actuator - 1].getPosition();
+        return actuators[actuator].getPosition();
+      } else {
+        return 0;
+      }
+    }
+
+    int getActuatorTarget(int actuator) {
+      if (actuatorIsValid(actuator)) {
+        return actuators[actuator].getTargetPosition();
       } else {
         return 0;
       }
@@ -302,7 +361,7 @@ class StewartPlatform {
 
     int getActuatorMaxPosition(int actuator) {
       if (actuatorIsValid(actuator)) {
-        return actuators[actuator - 1].getMaxPosition();
+        return actuators[actuator].getMaxPosition();
       } else {
         return 0;
       }
@@ -310,15 +369,31 @@ class StewartPlatform {
 
     int getActuatorMinPosition(int actuator) {
       if (actuatorIsValid(actuator)) {
-        return actuators[actuator - 1].getMinPosition();
+        return actuators[actuator].getMinPosition();
       } else {
         return 0;
       }
     }
 
+    bool getActuatorReady(int actuator) {
+      if (actuatorIsValid(actuator)) {
+        return actuators[actuator].isActuatorReady();
+      } else {
+        return 0;
+      }
+    }
+
+    bool getPlatformReady() {
+      isReady = true;
+      for (int i = 0; i < NUM_ACTUATORS; i++) {
+        isReady = isReady && actuators[i].isActuatorReady();
+      }
+      return isReady;
+    }
+
     private:
       bool actuatorIsValid(int actuator) {
-        return actuator > 0 && actuator <= NUM_ACTUATORS;
+        return actuator >= 0 && actuator < NUM_ACTUATORS;
       }
 };
 
@@ -329,24 +404,54 @@ StewartPlatform platform;
 void setup() {
   Serial.begin(9600);
 
-  // set the analog ref (5V)
-  analogReference(EXTERNAL); // make sure external reference is actually connected
+  // set the analog ref (5V regulator)
+  analogReference(EXTERNAL);
 
   platform.setup();
-
+  
   if (IS_GRAPHING) {
     Serial.println(platform.getActuatorMaxPosition(WHICH_ACTUATOR));
     Serial.println(platform.getActuatorMinPosition(WHICH_ACTUATOR));
-  } else {
-    Serial.end();
   }
+
+  /*
+  Serial.println("Actuator info");
+  for (int i = 1; i <= NUM_ACTUATORS; i++) {
+    Serial.print("Actuator "); Serial.print(i); 
+    Serial.print(" position: ");
+    Serial.println(platform.getActuatorPosition(i));
+    Serial.print("min: "); 
+    Serial.println(platform.getActuatorMinPosition(i));
+    Serial.print("max: "); 
+    Serial.println(platform.getActuatorMaxPosition(i));
+    Serial.print("target: ");
+    Serial.println(platform.getActuatorTarget(i));
+    Serial.print("ready: ");
+    Serial.println(platform.getActuatorReady(i));
+  } 
+  */
+  
 }
+
+bool isGoingUp = true;
+double high[] = {0.9, 0.9, 0.9, 0.9, 0.9, 0.9};
+double low[] = {0.1, 0.1, 0.1, 0.1, 0.1, 0.1};
 
 void loop() {
   platform.loop();
 
+  if(platform.getPlatformReady()) {
+    if(isGoingUp) {
+      platform.setPlatformLengths(high);
+      isGoingUp = false;
+    } else {
+      platform.setPlatformLengths(low);
+      isGoingUp = true;
+    }
+  }
+
   if (IS_GRAPHING) {
-    if (WHICH_ACTUATOR > 0 && WHICH_ACTUATOR <= NUM_ACTUATORS) {
+    if (WHICH_ACTUATOR >= 0 && WHICH_ACTUATOR < NUM_ACTUATORS) {
       if (IS_RAW_DATA) {
         Serial.println(platform.getActuatorRawPosition(WHICH_ACTUATOR));
       } else {
